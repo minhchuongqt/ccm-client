2b5f620c9abebd842be843d3fab2a0dd8e0e1842
minhchuongqt <minhchuongqt>
2019-05-16 21:12:11 +0700

    report

--- src/pages/ReportPage/ReportPage.js
+++ src/pages/ReportPage/ReportPage.js
@@ -1,35 +1,10 @@
 import React from "react";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
-import { Line } from 'react-chartjs-2';
+import SprintReport from "./SprintReport";
+import ProjectReport from "./ProjectReport";
 const ReportPage = props => {
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'Sprint 1',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
-  };
   return (
-    <div >
+    <div>
       <div>
         <Breadcrumb>
           <BreadcrumbItem active>Report</BreadcrumbItem>
@@ -37,22 +12,35 @@ const ReportPage = props => {
 
       </div>
       <div>
-        <div className="col-md-8">
-          <div className="box box-primary">
-            <div className="box-header with-border">
-              <h4 className="box-title">Sprint Report</h4>
-            </div>
-            <div className="box-body">
-            <Line data={data} />
+      </div>
+      <div>
+        <div>
+          <div className="nav-tabs-custom">
+            <ul className="nav nav-tabs">
+              <li className="active">
+                <a href="#tab_1" data-toggle="tab">
+                  Sprint Report
+                </a>
+              </li>
+              <li>
+                <a href="#tab_2" data-toggle="tab">
+                  Project Report
+                </a>
+              </li>
+            </ul>
+            <div className="tab-content">
+              <div className="tab-pane active" id="tab_1">
+                <SprintReport />
+              </div>
+              <div className="tab-pane" id="tab_2">
+              <ProjectReport />
+              </div>
             </div>
           </div>
         </div>
-        
       </div>
-
-
-    </div >
-  )
+    </div>
+  );
 };
 
 export default ReportPage;
